e2c6cf3ab05f271f7aedc8388c8b2adf2dea395f
AgustinNRios <122225722+AgustinNRios@users.noreply.github.com>
2025-09-10 20:40:57 -0300

    Test CI and CD

--- src/__tests__/smoke.test.tsx
+++ src/__tests__/smoke.test.tsx
@@ -4,6 +4,7 @@ import { render } from '@testing-library/react';
 describe('Smoke test', () => {
   it('renders a simple component', async () => {
     const { getByText } = render(<div>Hello test</div>);
+    throw new Error('Test error');
     expect(getByText('Hello test')).toBeInTheDocument();
   });
 });
