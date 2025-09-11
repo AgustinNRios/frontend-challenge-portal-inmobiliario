06dc9ae0e62cb8400c4753faa39d7071843f99f6
AgustinNRios <122225722+AgustinNRios@users.noreply.github.com>
2025-09-11 18:05:15 -0300

    Fixed layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+import Card from "@/components/Card";
 import Navbar from "@/components/Navbar";
+import SecondSection from "@/components/SecondSection";
+import SwitchButton from "@/components/SwitchButton.";
+import WeMakeItEasy from "@/components/WeMakeItEasy";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,21 +13,22 @@ export default function Home() {
       </div>
       <main className="flex flex-col gap-[32px] items-center w-full sm:items-start">
         <section className="flex flex-row w-full justify-between ">
-          <div className="w-50%">
+          <div className="w-50% bg-[#EFEFFB4D]">
             <h1 className="font-bold text-6xl max-w-[540px]">Buy, rent, or sell your property easily.</h1>
             <p className="max-w-[400px]">A great platform to buy, sell, or even rent your properties without any commisions.</p>
           </div>
-          <div className="relative w-full max-w-6/12 h-[800px]">
-            <Image 
+          <div className="relative w-full max-w-6/12 h-[800px] bg-[url('/mapaFondo.webp')]">
+            {/* <Image 
               src="/mapaFondo.webp" 
               alt="mapaFondo" 
               fill
               className="object-cover"
-            />
+            /> */}
+            <Card/>
           </div>
         </section>
-        <section>
-
+        <section className="flex flex-col sm:flex-row w-full justify-between gap-8 px-6">
+          <SecondSection/>
         </section>
       </main>
       {/* <footer className="flex gap-[24px] flex-wrap items-center justify-center">
