f1f951978d160538d6100bb3d7a5c5a4b8087c94
AgustinNRios <122225722+AgustinNRios@users.noreply.github.com>
2025-09-11 23:28:04 -0300

    Fixed css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 import Card from "@/components/Card";
-import Navbar from "@/components/Navbar";
+import NavbarComponent from "@/components/Navbar";
 import SecondSection from "@/components/SecondSection";
 import StatisticsNumber from "@/components/StatisticsNumber";
 
 export default function Home() {
   return (
-    <div className="font-sans  items-center justify-items-center max-w-[1440px] min-h-screen pb-8 gap-16 ">
+    <div className="font-sans  items-center justify-items-center  min-h-screen pb-8 gap-16 "> 
       <div className="w-full p-6">
-        <Navbar/>
+        <NavbarComponent/>
       </div>
-      <main className="flex flex-col gap-[32px] items-center w-full sm:items-start">
+      <main className="flex flex-col gap-16 items-center w-full sm:items-start">
         <section className="flex flex-row w-full justify-between ">
-          <div className="flex flex-col mx-auto gap-8 w-50% bg-[#EFEFFB4D]">
+          <div className="flex flex-col justify-center mx-auto gap-8 w-50% bg-[#EFEFFB4D]">
             <h1 className="font-bold text-6xl max-w-[480px]">Buy, rent, or sell your property easily.</h1>
-            <p className="max-w-[400px]">A great platform to buy, sell, or even rent your properties without any commisions.</p>
-            <div className="flex flex-row gap-12">
+            <p className="max-w-[440px] text-xl font-medium">A great platform to buy, sell, or even rent your properties without any commisions.</p>
+            <div className="flex flex-row gap-16">
               <StatisticsNumber
                 number={50}
                 text="renters"
@@ -41,7 +41,7 @@ export default function Home() {
             />
           </div>
         </section>
-        <section className="flex flex-col sm:flex-row w-full justify-between gap-8 px-6">
+        <section className="flex flex-col sm:flex-row w-full justify-between gap-8 px-6 pb-20">
           <SecondSection/>
         </section>
       </main>
