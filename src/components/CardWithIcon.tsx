e0f9c810e5a11557d377f7f9fa60c27d88544c38
AgustinNRios <122225722+AgustinNRios@users.noreply.github.com>
2025-09-11 23:27:33 -0300

    Added Card comp to Section

---
+++ src/components/CardWithIcon.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+interface Props {
+    img: string,
+    title: string,
+    text: string,
+    isIn?: boolean,
+}
+
+export default function CardWithIcon({img, title, text, isIn=true}: Props) {
+    return (
+        <div className={` bg-white border-2 rounded-lg border-[#E0DEF7] min-w-[350px] ${isIn ? "flex flex-row gap-5 p-5" : "flex relative p-5 px-8"} `}>
+            <div className={`${isIn ? "" : "absolute -top-4/12 right-5"}`}>
+                <Image
+                    src={img}
+                    alt="icono"
+                    width={64}
+                    height={64}
+                />
+            </div>
+            <div className="flex flex-col gap-1">
+                <h3 className="text-[#100A55] text-xl font-bold">{title}</h3>
+                <p className="text-gray-600">{text}</p>
+            </div>
+        </div>
+    )
+}
--- src/components/SecondSection.tsx
+++ src/components/SecondSection.tsx
@@ -4,19 +4,35 @@ import { useState } from "react";
 import SwitchButton from "./SwitchButton.";
 import WeMakeItEasy from "./WeMakeItEasy";
 import Image from "next/image";
+import CardWithIcon from "./CardWithIcon";
 
 export default function SecondSection() {
     const [isOn, setIsOn] = useState(true);
 
     return (
-        <div className="flex flex-row items-center justify-center w-full gap-16">
-            <div>
+        <div className="relative flex flex-row items-center justify-center w-full gap-16">
+            <div className="relative ">
                 <Image
                     src={"/casa2.webp"}
                     alt="casa2"
                     width={500}
                     height={600}
                 />
+                <div className="absolute top-1/12 left-[-6rem]">
+                    <CardWithIcon
+                        img="/IconPlay.svg"
+                        title="Virtual home tour"
+                        text="We provide you with virtual tour"
+                    />
+                </div>
+                <div className="absolute -bottom-1/12 left-[4rem]">
+                    <CardWithIcon
+                        img="/IconHouse.svg"
+                        title="Find the best deal"
+                        text="Browse thousands of properties"
+                        isIn={false}
+                    />
+                </div>
             </div>
             <div>
                 <SwitchButton
