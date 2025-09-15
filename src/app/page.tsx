1ad34e2de71d35f9f841a31b11caaf87e13e4ea3
AgustinNRios <122225722+AgustinNRios@users.noreply.github.com>
2025-09-15 16:11:05 -0300

    Added animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import PropertyPoint from "@/Domains/Properties/components/PropertyPoint";
 import SecondSection from "@/components/withState/SecondSection";
 import { propertiesMock } from "@/Domains/Properties/Mocks/PropertiesMock";
 import Image from "next/image";
+import FadeInOnView from "@/animations/FadeInOnView";
 
 export default function Home() {
   return (
@@ -11,68 +12,64 @@ export default function Home() {
           <div className="flex flex-col pt-7 lg:pt-0 items-center lg:flex-row w-full overflow-w-hidden px-5 justify-center lg:items-start gap-6">
             <div className="flex justify-end w-full mt-18 lg:w-[701px]">
               <div className="w-full lg:w-[544px] z-20">
-                <FirstSection/>
+                <FadeInOnView>
+                  <FirstSection/>
+                </FadeInOnView>
               </div>
             </div>
             <div className="overflow-auto max-w-[701px] max-h-[846px] scale-50 sm:scale-75 lg:scale-100">
-              <div className="relative w-[681px] h-[826px]">
-                <Image
-                  src={"/mapaFondo.webp"}
-                  alt="camino desde la posicion actual"
-                  width={701}
-                  height={846}
-                  className="w-[701px] h-[826px]"
-                  priority
-                />
-                {/* <div className="absolute top-[55px] left-[285px]">
+              <FadeInOnView>
+                <div className="relative w-[681px] h-[826px]">
                   <Image
-                    src={"/circle.svg"}
-                    alt="posicion actual"
-                    width={56}
-                    height={56}
-                  />
-                </div> */}
-                <div className="absolute top-[-15px] left-[340px]">
-                  <PropertyPoint popoverPlacement="left-start" property={propertiesMock[0]} isOpen={true} scale={false}/>
-                </div>
-                <div className="absolute top-[117px] left-[550px]">
-                  <PropertyPoint popoverPlacement="bottom" property={propertiesMock[3]} size={2} isOpen={false} opacity={30}/>
-                </div>
-                <div className="absolute top-[200px] left-[385px]">
-                  <PropertyPoint popoverPlacement="left" property={propertiesMock[2]} isOpen={false} size={3}/>
-                </div>
-                <div className="absolute top-[365px] left-[375px]">
-                  <PropertyPoint popoverPlacement="right-start" property={propertiesMock[1]} isOpen={true} opacity={50}/>
-                </div>
-                <div className="absolute top-[470px] left-[110px]">
-                  <PropertyPoint popoverPlacement="top" property={propertiesMock[4]} isOpen={false} size={3} opacity={30}/>
-                </div>
-                <div className="absolute top-[530px] left-[250px]">
-                  <PropertyPoint popoverPlacement="top-start" property={propertiesMock[5]} isOpen={false} size={2} opacity={30}/>
-                </div>
-                <div className="absolute top-[515px] left-[610px]">
-                  <PropertyPoint popoverPlacement="left" property={propertiesMock[6]} isOpen={false} size={2}/>
-                </div>
-                <div className="absolute top-[665px] left-[280px]">
-                  <PropertyPoint popoverPlacement="top" property={propertiesMock[7]} isOpen={false}/>
-                </div>
-                <div className="absolute top-[102px] left-[385px]">
-                  <Image
-                    src={"/path.svg"}
+                    src={"/mapaFondo.webp"}
                     alt="camino desde la posicion actual"
-                    width={215}
-                    height={307}
-                    className="!w-[215px] !h-[307px]"
+                    width={701}
+                    height={846}
+                    className="w-[701px] h-[826px]"
+                    priority
                   />
+                  <div className="absolute top-[-15px] left-[340px]">
+                    <PropertyPoint popoverPlacement="left-start" property={propertiesMock[0]} isOpen={true} scale={false}/>
+                  </div>
+                  <div className="absolute top-[117px] left-[550px]">
+                    <PropertyPoint popoverPlacement="bottom" property={propertiesMock[3]} size={2} isOpen={false} opacity={30}/>
+                  </div>
+                  <div className="absolute top-[200px] left-[385px]">
+                    <PropertyPoint popoverPlacement="left" property={propertiesMock[2]} isOpen={false} size={3}/>
+                  </div>
+                  <div className="absolute top-[365px] left-[375px]">
+                    <PropertyPoint popoverPlacement="right" property={propertiesMock[1]} isOpen={true} opacity={50}/>
+                  </div>
+                  <div className="absolute top-[470px] left-[110px]">
+                    <PropertyPoint popoverPlacement="top" property={propertiesMock[4]} isOpen={false} size={3} opacity={30}/>
+                  </div>
+                  <div className="absolute top-[530px] left-[250px]">
+                    <PropertyPoint popoverPlacement="top-start" property={propertiesMock[5]} isOpen={false} size={2} opacity={30}/>
+                  </div>
+                  <div className="absolute top-[515px] left-[610px]">
+                    <PropertyPoint popoverPlacement="left" property={propertiesMock[6]} isOpen={false} size={2}/>
+                  </div>
+                  <div className="absolute top-[665px] left-[280px]">
+                    <PropertyPoint popoverPlacement="top" property={propertiesMock[7]} isOpen={false}/>
+                  </div>
+                  <div className="absolute top-[102px] left-[385px]">
+                    <Image
+                      src={"/path.svg"}
+                      alt="camino desde la posicion actual"
+                      width={215}
+                      height={307}
+                      className="!w-[215px] !h-[307px]"
+                    />
+                  </div>
                 </div>
-              </div>
+              </FadeInOnView>
             </div>
-            {/* <div className="relative w-full max-w-[701px] h-[846px] max-h-[846px] bg-[url('/mapaFondo.webp')] bg-cover overflow-hidden shrink-0" role="img" aria-label="Mapa con puntos de propiedades">
-            </div> */}
           </div>
         </section>
         <section className="flex flex-col sm:flex-row w-full justify-between gap-8 px-6 pb-20">
-          <SecondSection/>
+          <FadeInOnView>
+            <SecondSection/>
+          </FadeInOnView>
         </section>
       </main>
   );
