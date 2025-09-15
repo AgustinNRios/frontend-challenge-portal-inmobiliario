c61c05e99c2476847255e43e1ee1e723941762d2
AgustinNRios <122225722+AgustinNRios@users.noreply.github.com>
2025-09-15 14:56:40 -0300

    Fixed SecondSection css

--- src/components/WeMakeItEasy.tsx
+++ src/components/WeMakeItEasy.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 export default function WeMakeItEasy({tenants}: Props) {
     return (
-        <div className="">
+        <div className="pt-5">
             <h1 className="text-4xl font-bold max-w-[440px]">{tenants ? "We make it easy for tenants and landlords." : "We make it easy for landlords and tenants."}</h1>
-            <p className="max-w-[400px] my-6">{tenants ? "Whether it’s selling your current home, getting financing, or buying a new home, we make it easy  and efficient. The best part? you’ll save a bunch of money and time with our services." : "adsajdfhakvbfbvladbdkl. The best part? you’ll save a bunch of money and time with our services."}</p>
+            <p className="max-w-[400px] my-9 h-[104px]">{tenants ? "Whether it’s selling your current home, getting financing, or buying a new home, we make it easy  and efficient. The best part? you’ll save a bunch of money and time with our services." : "adsajdfhakvbfbvladbdkl. The best part? you’ll save a bunch of money and time with our services."}</p>
             <MainButton
                 text="See more"
                 bgColor="bg-primary"
--- src/components/withState/SecondSection.tsx
+++ src/components/withState/SecondSection.tsx
@@ -15,17 +15,17 @@ export default function SecondSection() {
                 <Image
                     src={"/casa2.webp"}
                     alt="casa2"
-                    width={500}
+                    width={544}
                     height={600}
                 />
-                <div className="absolute top-1/12 left-[0rem] lg:left-[-1rem] xl:left-[-6rem] transition-all duration-300 ease-in-out">
+                <div className="absolute top-1/12 left-[6rem] lg:left-[-1rem] xl:left-[-6rem] transition-all duration-300 ease-in-out">
                     <CardWithIcon
                         img="/IconPlay.svg"
                         title="Virtual home tour"
                         text="We provide you with virtual tour"
                     />
                 </div>
-                <div className="absolute bottom-1/12 lg:-bottom-1/12 left-[0rem] lg:left-[4rem] transition-all duration-300 ease-in-out">
+                <div className="absolute bottom-1/12 lg:-bottom-1/12 left-[7rem] lg:left-[7rem] transition-all duration-300 ease-in-out">
                     <CardWithIcon
                         img="/IconHouse.svg"
                         title="Find the best deal"
