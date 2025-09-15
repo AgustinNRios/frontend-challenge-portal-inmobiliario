fd4103668af51ca65ba8ffff9f0db2c77e40af60
AgustinNRios <122225722+AgustinNRios@users.noreply.github.com>
2025-09-15 16:18:55 -0300

    fixed animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,58 +18,54 @@ export default function Home() {
               </div>
             </div>
             <div className="overflow-auto max-w-[701px] max-h-[846px] scale-50 sm:scale-75 lg:scale-100">
-              <FadeInOnView>
-                <div className="relative w-[681px] h-[826px]">
-                  <Image
-                    src={"/mapaFondo.webp"}
-                    alt="camino desde la posicion actual"
-                    width={701}
-                    height={846}
-                    className="w-[701px] h-[826px]"
-                    priority
-                  />
-                  <div className="absolute top-[-15px] left-[340px]">
-                    <PropertyPoint popoverPlacement="left-start" property={propertiesMock[0]} isOpen={true} scale={false}/>
-                  </div>
-                  <div className="absolute top-[117px] left-[550px]">
-                    <PropertyPoint popoverPlacement="bottom" property={propertiesMock[3]} size={2} isOpen={false} opacity={30}/>
-                  </div>
-                  <div className="absolute top-[200px] left-[385px]">
-                    <PropertyPoint popoverPlacement="left" property={propertiesMock[2]} isOpen={false} size={3}/>
-                  </div>
-                  <div className="absolute top-[365px] left-[375px]">
-                    <PropertyPoint popoverPlacement="right" property={propertiesMock[1]} isOpen={true} opacity={50}/>
-                  </div>
-                  <div className="absolute top-[470px] left-[110px]">
-                    <PropertyPoint popoverPlacement="top" property={propertiesMock[4]} isOpen={false} size={3} opacity={30}/>
-                  </div>
-                  <div className="absolute top-[530px] left-[250px]">
-                    <PropertyPoint popoverPlacement="top-start" property={propertiesMock[5]} isOpen={false} size={2} opacity={30}/>
-                  </div>
-                  <div className="absolute top-[515px] left-[610px]">
-                    <PropertyPoint popoverPlacement="left" property={propertiesMock[6]} isOpen={false} size={2}/>
-                  </div>
-                  <div className="absolute top-[665px] left-[280px]">
-                    <PropertyPoint popoverPlacement="top" property={propertiesMock[7]} isOpen={false}/>
-                  </div>
-                  <div className="absolute top-[102px] left-[385px]">
-                    <Image
-                      src={"/path.svg"}
-                      alt="camino desde la posicion actual"
-                      width={215}
-                      height={307}
-                      className="!w-[215px] !h-[307px]"
-                    />
-                  </div>
+              <div className="relative w-[681px] h-[826px]">
+                <Image
+                  src={"/mapaFondo.webp"}
+                  alt="camino desde la posicion actual"
+                  width={701}
+                  height={846}
+                  className="w-[701px] h-[826px]"
+                  priority
+                />
+                <div className="absolute top-[-15px] left-[340px]">
+                  <PropertyPoint popoverPlacement="left-start" property={propertiesMock[0]} isOpen={true} scale={false}/>
                 </div>
-              </FadeInOnView>
+                <div className="absolute top-[117px] left-[550px]">
+                  <PropertyPoint popoverPlacement="bottom" property={propertiesMock[3]} size={2} isOpen={false} opacity={30}/>
+                </div>
+                <div className="absolute top-[200px] left-[385px]">
+                  <PropertyPoint popoverPlacement="left" property={propertiesMock[2]} isOpen={false} size={3}/>
+                </div>
+                <div className="absolute top-[365px] left-[375px]">
+                  <PropertyPoint popoverPlacement="right" property={propertiesMock[1]} isOpen={true} opacity={50}/>
+                </div>
+                <div className="absolute top-[470px] left-[110px]">
+                  <PropertyPoint popoverPlacement="top" property={propertiesMock[4]} isOpen={false} size={3} opacity={30}/>
+                </div>
+                <div className="absolute top-[530px] left-[250px]">
+                  <PropertyPoint popoverPlacement="top-start" property={propertiesMock[5]} isOpen={false} size={2} opacity={30}/>
+                </div>
+                <div className="absolute top-[515px] left-[610px]">
+                  <PropertyPoint popoverPlacement="left" property={propertiesMock[6]} isOpen={false} size={2}/>
+                </div>
+                <div className="absolute top-[665px] left-[280px]">
+                  <PropertyPoint popoverPlacement="top" property={propertiesMock[7]} isOpen={false}/>
+                </div>
+                <div className="absolute top-[102px] left-[385px]">
+                  <Image
+                    src={"/path.svg"}
+                    alt="camino desde la posicion actual"
+                    width={215}
+                    height={307}
+                    className="!w-[215px] !h-[307px]"
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </section>
         <section className="flex flex-col sm:flex-row w-full justify-between gap-8 px-6 pb-20">
-          <FadeInOnView>
             <SecondSection/>
-          </FadeInOnView>
         </section>
       </main>
   );
--- src/components/withState/SecondSection.tsx
+++ src/components/withState/SecondSection.tsx
@@ -6,48 +6,53 @@ import WeMakeItEasy from "../WeMakeItEasy";
 import Image from "next/image";
 import CardWithIcon from "../CardWithIcon";
 import TiltCard from "@/animations/TiltCard";
+import FadeInOnView from "@/animations/FadeInOnView";
 
 export default function SecondSection() {
     const [isOn, setIsOn] = useState(true);
 
     return (
         <div className="relative flex flex-wrap-reverse  lg:flex-row items-center justify-center w-full gap-16 lg:gap-32">
-            <div className="relative ">
-                <Image
-                    src={"/casa2.webp"}
-                    alt="casa2"
-                    width={544}
-                    height={600}
-                />
-                <div className="absolute top-1/12 left-[0rem] md:left-[6rem] lg:left-[-1rem] xl:left-[-6rem] transition-all duration-300 ease-in-out">
-                    <TiltCard>
-                        <CardWithIcon
-                            img="/IconPlay.svg"
-                            title="Virtual home tour"
-                            text="We provide you with virtual tour"
-                        />
-                    </TiltCard>
+            <FadeInOnView>
+                <div className="relative ">
+                    <Image
+                        src={"/casa2.webp"}
+                        alt="casa2"
+                        width={544}
+                        height={600}
+                    />
+                    <div className="absolute top-1/12 left-[0rem] md:left-[6rem] lg:left-[-1rem] xl:left-[-6rem] transition-all duration-300 ease-in-out">
+                        <TiltCard>
+                            <CardWithIcon
+                                img="/IconPlay.svg"
+                                title="Virtual home tour"
+                                text="We provide you with virtual tour"
+                            />
+                        </TiltCard>
+                    </div>
+                    <div className="absolute bottom-1/12 lg:-bottom-1/12 left-[0.5rem] md:left-[7rem] lg:left-[7rem] transition-all duration-300 ease-in-out">
+                        <TiltCard>
+                            <CardWithIcon
+                                img="/IconHouse.svg"
+                                title="Find the best deal"
+                                text="Browse thousands of properties"
+                                isIn={false}
+                            />
+                        </TiltCard>
+                    </div>
                 </div>
-                <div className="absolute bottom-1/12 lg:-bottom-1/12 left-[0.5rem] md:left-[7rem] lg:left-[7rem] transition-all duration-300 ease-in-out">
-                    <TiltCard>
-                        <CardWithIcon
-                            img="/IconHouse.svg"
-                            title="Find the best deal"
-                            text="Browse thousands of properties"
-                            isIn={false}
-                        />
-                    </TiltCard>
+            </FadeInOnView>
+            <FadeInOnView>
+                <div className="min-h-[350px]">
+                    <SwitchButton
+                        isOn={isOn}
+                        setIsOn={() => {setIsOn(!isOn)}}
+                    />
+                    <WeMakeItEasy
+                        tenants={isOn}
+                    />
                 </div>
-            </div>
-            <div className="min-h-[350px]">
-                <SwitchButton
-                    isOn={isOn}
-                    setIsOn={() => {setIsOn(!isOn)}}
-                />
-                <WeMakeItEasy
-                    tenants={isOn}
-                />
-            </div>
+            </FadeInOnView>
         </div>
     )
 }
