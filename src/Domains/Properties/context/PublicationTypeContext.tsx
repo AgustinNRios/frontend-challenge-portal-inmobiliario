fd8b064db1a8da0ab493142be0d81394e8a28f31
AgustinNRios <122225722+AgustinNRios@users.noreply.github.com>
2025-09-22 14:33:58 -0300

    test coveralls

--- src/Domains/Properties/context/PublicationTypeContext.tsx
+++ src/Domains/Properties/context/PublicationTypeContext.tsx
@@ -1,23 +1,23 @@
-import { createContext, useState, ReactNode, Dispatch, SetStateAction } from "react";
-import { PropertyType } from "../model/Property";
+// import { createContext, useState, ReactNode, Dispatch, SetStateAction } from "react";
+// import { PropertyType } from "../model/Property";
 
-type PublicationType = PropertyType.Rent | PropertyType.Buy | PropertyType.Sale;
+// type PublicationType = PropertyType.Rent | PropertyType.Buy | PropertyType.Sale;
 
-interface PublicationTypeContextProps {
-    publicationType: PublicationType;
-    setPublicationType: Dispatch<SetStateAction<PublicationType>>;
-}
+// interface PublicationTypeContextProps {
+//     publicationType: PublicationType;
+//     setPublicationType: Dispatch<SetStateAction<PublicationType>>;
+// }
 
-export const PublicationTypeContext = createContext<PublicationTypeContextProps>({
-    publicationType: PropertyType.Rent,
-    setPublicationType: () => {},
-});
+// export const PublicationTypeContext = createContext<PublicationTypeContextProps>({
+//     publicationType: PropertyType.Rent,
+//     setPublicationType: () => {},
+// });
 
-export const PublicationTypeProvider = ({ children }: { children: ReactNode }) => {
-    const [publicationType, setPublicationType] = useState<PublicationType>(PropertyType.Rent);
-    return (
-        <PublicationTypeContext.Provider value={{ publicationType, setPublicationType }}>
-            {children}
-        </PublicationTypeContext.Provider>
-    );
-};
+// export const PublicationTypeProvider = ({ children }: { children: ReactNode }) => {
+//     const [publicationType, setPublicationType] = useState<PublicationType>(PropertyType.Rent);
+//     return (
+//         <PublicationTypeContext.Provider value={{ publicationType, setPublicationType }}>
+//             {children}
+//         </PublicationTypeContext.Provider>
+//     );
+// };
